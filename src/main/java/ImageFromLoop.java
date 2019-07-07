31c978694fddbfc50d4b5ed49213c8b46acd16b8
DamianG <damian_glonek@o2.pl>
2019-07-07 12:14:03 +0200

    commit

--- src/main/java/ImageFromLoop.java
+++ src/main/java/ImageFromLoop.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ImageFromLoop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("give me the number:");
+        System.out.println("give me the money:");
         int size = sc.nextInt();
 //      int size = 15;
 
